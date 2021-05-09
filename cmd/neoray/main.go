ad06e8eebb1d318fcc10c53f50c7683c0e6699d6
hismailbulut <bulut-konya@hotmail.com>
2021-05-09 23:39:27 +0300

    Update main.go

--- cmd/neoray/main.go
+++ cmd/neoray/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+)
+
+const (
+	NEORAY_NAME          = "Neoray"
+	NEORAY_VERSION_MAJOR = 0
+	NEORAY_VERSION_MINOR = 0
+	NEORAY_VERSION_PATCH = 1
+	NEORAY_WEBPAGE       = "github.com/hismailbulut/Neoray"
+	NEORAY_LICENSE       = "GPLv3"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
 func main() {
 	editor := Editor{}
 	editor.Initialize()
