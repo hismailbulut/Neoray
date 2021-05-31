7133cf274cb579d17f7c5934ef10993520bf997b
hismailbulut <bulut-konya@hotmail.com>
2021-06-01 01:49:23 +0300

    Update animation.go

--- cmd/neoray/animation.go
+++ cmd/neoray/animation.go
@@ -8,6 +8,8 @@ type Animation struct {
 	lifeTime float32
 }
 
+const AnimationFinishTolerance = 0.1
+
 // Lifetime is the life of the animation. Animation speed is depends of the
 // delta time and lifetime. For lifeTime parameter, 1.0 value is 1 seconds
 func CreateAnimation(from, target f32vec2, lifeTime float32) Animation {
@@ -23,7 +25,7 @@ func CreateAnimation(from, target f32vec2, lifeTime float32) Animation {
 func (anim *Animation) GetCurrentStep(deltaTime float32) (f32vec2, bool) {
 	anim.current.X += (anim.target.X - anim.current.X) / (anim.lifeTime / deltaTime)
 	anim.current.Y += (anim.target.Y - anim.current.Y) / (anim.lifeTime / deltaTime)
-	finishedX := math.Abs(float64(anim.target.X-anim.current.X)) < 0.1
-	finishedY := math.Abs(float64(anim.target.Y-anim.current.Y)) < 0.1
+	finishedX := math.Abs(float64(anim.target.X-anim.current.X)) < AnimationFinishTolerance
+	finishedY := math.Abs(float64(anim.target.Y-anim.current.Y)) < AnimationFinishTolerance
 	return anim.current, finishedX && finishedY
 }
