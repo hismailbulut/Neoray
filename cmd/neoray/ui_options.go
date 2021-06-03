b4726c946ae94a9665a363d3e8c3c79c6edaadf4
hismailbulut <bulut-konya@hotmail.com>
2021-06-03 19:04:21 +0300

    Create ui_options.go

---
+++ cmd/neoray/ui_options.go
@@ -0,0 +1,14 @@
+package main
+
+type UIOptions struct {
+	arabicshape   bool
+	ambiwidth     string
+	emoji         bool
+	guifont       string
+	guifontset    string
+	guifontwide   string
+	linespace     int
+	pumblend      int
+	showtabline   int
+	termguicolors bool
+}
