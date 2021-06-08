292b87b12c48dab9798dd4586655a323211610da
hismailbulut <bulut-konya@hotmail.com>
2021-06-08 23:12:49 +0300

    Update utils.go

--- cmd/neoray/utils.go
+++ cmd/neoray/utils.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"runtime/debug"
-	"sync"
-	"time"
+	"sync/atomic"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -16,7 +12,6 @@ var (
 	COLOR_TRANSPARENT = sdl.Color{R: 0, G: 0, B: 0, A: 0}
 )
 
-// Math
 type f32vec2 struct {
 	X, Y float32
 }
@@ -90,131 +85,33 @@ func triangulateFRect(rect *sdl.FRect) [4]f32vec2 {
 	}
 }
 
+func ortho(top, left, right, bottom, near, far float32) [16]float32 {
+	rml, tmb, fmn := (right - left), (top - bottom), (far - near)
+	return [16]float32{
+		float32(2. / rml), 0, 0, 0, // 1
+		0, float32(2. / tmb), 0, 0, // 2
+		0, 0, float32(-2. / fmn), 0, // 3
+		float32(-(right + left) / rml), // 4
+		float32(-(top + bottom) / tmb),
+		float32(-(far + near) / fmn), 1}
+}
+
 func has_flag_u16(val, flag uint16) bool {
 	return val&flag != 0
 }
 
-// DEBUGGING UTILITIES
-type FunctionMeasure struct {
-	totalCall int64
-	totalTime time.Duration
+func atomicSetBool(b *int32, value bool) {
+	var val int32 = 0
+	if value == true {
+		val = 1
+	}
+	atomic.StoreInt32(b, val)
 }
 
-var measure_averages map[string]FunctionMeasure
-var measure_averages_mutex sync.Mutex
-
-func init_function_time_tracker() {
-	measure_averages = make(map[string]FunctionMeasure)
-}
-
-func measure_execution_time(name string) func() {
-	now := time.Now()
-	return func() {
-		elapsed := time.Since(now)
-		measure_averages_mutex.Lock()
-		defer measure_averages_mutex.Unlock()
-		if val, ok := measure_averages[name]; ok == true {
-			val.totalCall++
-			val.totalTime += elapsed
-			measure_averages[name] = val
-		} else {
-			measure_averages[name] = FunctionMeasure{
-				totalCall: 1,
-				totalTime: elapsed,
-			}
-		}
+func atomicGetBool(b *int32) bool {
+	val := atomic.LoadInt32(b)
+	if val == 0 {
+		return false
 	}
-}
-
-func close_function_time_tracker() {
-	for key, val := range measure_averages {
-		log_message(LOG_LEVEL_DEBUG, LOG_TYPE_PERFORMANCE,
-			key, "Calls:", val.totalCall, "Time:", val.totalTime, "Average:", val.totalTime/time.Duration(val.totalCall))
-	}
-}
-
-// Logger
-const MINIMUM_LOG_LEVEL = LOG_LEVEL_DEBUG
-const (
-	// log levels
-	LOG_LEVEL_DEBUG = iota
-	LOG_LEVEL_WARN
-	LOG_LEVEL_ERROR
-	LOG_LEVEL_FATAL
-	// log types
-	LOG_TYPE_NVIM
-	LOG_TYPE_NEORAY
-	LOG_TYPE_RENDERER
-	LOG_TYPE_PERFORMANCE
-	// NOTE: Delete all debug type messages on release build.
-	LOG_TYPE_DEBUG_MESSAGE
-)
-
-func log_message(log_level, log_type int, message ...interface{}) {
-	if log_level < MINIMUM_LOG_LEVEL {
-		return
-	}
-
-	log_string := " "
-	debug_type := false
-	switch log_type {
-	case LOG_TYPE_NVIM:
-		log_string += "[NVIM]"
-	case LOG_TYPE_NEORAY:
-		log_string += "[NEORAY]"
-	case LOG_TYPE_RENDERER:
-		log_string += "[RENDERER]"
-	case LOG_TYPE_PERFORMANCE:
-		log_string += "[PERFORMANCE]"
-	case LOG_TYPE_DEBUG_MESSAGE:
-		log_string += ">>"
-		debug_type = true
-	default:
-		return
-	}
-
-	err := false
-	fatal := false
-	log_string += " "
-	if !debug_type {
-		switch log_level {
-		case LOG_LEVEL_DEBUG:
-			log_string += "DEBUG:"
-		case LOG_LEVEL_WARN:
-			log_string += "WARNING:"
-		case LOG_LEVEL_ERROR:
-			log_string += "ERROR:"
-			err = true
-		case LOG_LEVEL_FATAL:
-			log_string += "FATAL:"
-			fatal = true
-		default:
-			return
-		}
-	}
-	log_string += " "
-	for _, msg := range message {
-		log_string += fmt.Sprint(msg)
-		log_string += " "
-	}
-
-	if fatal {
-		fmt.Printf("\n")
-		debug.PrintStack()
-		log.Fatalln(log_string)
-	} else if err {
-		log.Println(log_string)
-	} else {
-		log.Println(log_string)
-	}
-}
-
-func log_debug_msg(message ...interface{}) {
-	log_message(LOG_LEVEL_DEBUG, LOG_TYPE_DEBUG_MESSAGE, message...)
-}
-
-func assert(cond bool, message ...interface{}) {
-	if cond == false {
-		log_message(LOG_LEVEL_FATAL, LOG_TYPE_NEORAY, "Assertion Failed:", message)
-	}
+	return true
 }
