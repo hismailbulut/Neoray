4990e7833ba58dcccb1e86ed20b7e7382678cfc7
hismailbulut <bulut-konya@hotmail.com>
2021-06-10 01:22:14 +0300

    Update ui_options.go

--- cmd/neoray/ui_options.go
+++ cmd/neoray/ui_options.go
@@ -21,43 +21,37 @@ type UIOptions struct {
 	smoothcursor bool
 }
 
-func (options *UIOptions) SetGuiFont(font string) {
+func (options *UIOptions) SetGuiFont(newGuiFont string) {
 	// Load Font
-	if font != "" && font != " " && font != options.guifont {
-		options.setUserFont(font)
-		options.guifont = font
-	}
-}
-
-func (options *UIOptions) setUserFont(guifont string) {
-	options.guifont = guifont
-	size := DEFAULT_FONT_SIZE
-	// treat underlines like whitespaces
-	guifont = strings.ReplaceAll(guifont, "_", " ")
-	// parse font options
-	fontOptions := strings.Split(guifont, ":")
-	name := fontOptions[0]
-	for _, opt := range fontOptions[1:] {
-		// TODO:h guifont
-		if opt[0] == 'h' {
-			tsize, err := strconv.Atoi(opt[1:])
-			if err == nil {
-				size = tsize
+	if newGuiFont != "" && newGuiFont != " " && newGuiFont != options.guifont {
+		options.guifont = newGuiFont
+		size := DEFAULT_FONT_SIZE
+		// treat underlines like whitespaces
+		newGuiFont = strings.ReplaceAll(newGuiFont, "_", " ")
+		// parse font options
+		fontOptions := strings.Split(newGuiFont, ":")
+		name := fontOptions[0]
+		for _, opt := range fontOptions[1:] {
+			if opt[0] == 'h' {
+				tsize, err := strconv.Atoi(opt[1:])
+				if err == nil {
+					size = tsize
+				}
 			}
 		}
+		// if this is the system default font, we already loaded it
+		if name == systemFontDefault {
+			log_debug_msg("Already loaded system font.")
+			// Unload current font. Because user wants system default.
+			EditorSingleton.renderer.SetFont(Font{})
+			return
+		}
+		// Create and set renderers font.
+		log_debug_msg("Loading font:", name)
+		font, ok := CreateFont(name, float32(size))
+		if !ok {
+			return
+		}
+		EditorSingleton.renderer.SetFont(font)
 	}
-	// if this is the system default font, we already loaded it
-	if name == system_default_fontname {
-		log_debug_msg("Already loaded system font.")
-		// Unload current font
-		EditorSingleton.renderer.SetFont(Font{})
-		return
-	}
-	// create and set renderers font
-	log_debug_msg("Loading font:", name)
-	font, ok := CreateFont(name, float32(size))
-	if !ok {
-		return
-	}
-	EditorSingleton.renderer.SetFont(font)
 }
