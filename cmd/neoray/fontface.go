36bddbc63a565f0ca97eeee70ddef3cd23a30f86
hismailbulut <bulut-konya@hotmail.com>
2021-06-15 15:14:06 +0300

    Resize fonts instead of reloading

--- cmd/neoray/fontface.go
+++ cmd/neoray/fontface.go
@@ -7,13 +7,14 @@ import (
 
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
+	"golang.org/x/image/font/sfnt"
 	"golang.org/x/image/math/fixed"
 )
 
 type FontFace struct {
-	handle font.Face
-	// fontHandle *sfnt.Font
-	drawer font.Drawer
+	handle     font.Face
+	fontHandle *sfnt.Font
+	buffer     sfnt.Buffer
 
 	loaded  bool
 	advance int
@@ -47,25 +48,52 @@ func CreateFontFace(fileName string, size int) (FontFace, error) {
 	}
 
 	fontFace := FontFace{
-		handle:  face,
-		loaded:  true,
-		advance: advance.Ceil(),
-		ascent:  face.Metrics().Ascent.Floor(),
-		descent: face.Metrics().Descent.Floor(),
+		handle:     face,
+		fontHandle: sfont,
+		loaded:     true,
+		advance:    advance.Ceil(),
+		ascent:     face.Metrics().Ascent.Floor(),
+		descent:    face.Metrics().Descent.Floor(),
 	}
-	fontFace.drawer = font.Drawer{
-		Src:  image.White,
-		Face: fontFace.handle,
-	}
+
 	return fontFace, nil
+}
+
+func (fontFace *FontFace) Resize(newsize int) error {
+	face, err := opentype.NewFace(fontFace.fontHandle, &opentype.FaceOptions{
+		Size:    float64(newsize),
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		return fmt.Errorf("Failed to create font face: %s\n", err)
+	}
+	advance, ok := face.GlyphAdvance('m')
+	if !ok {
+		return fmt.Errorf("Failed to get glyph advance!")
+	}
+	fontFace.handle = face
+	fontFace.advance = advance.Ceil()
+	fontFace.ascent = face.Metrics().Ascent.Floor()
+	fontFace.descent = face.Metrics().Descent.Floor()
+	return nil
+}
+
+func (fontFace *FontFace) IsDrawable(c string) bool {
+	char := []rune(c)[0]
+	i, err := fontFace.fontHandle.GlyphIndex(&fontFace.buffer, char)
+	return i != 0 || err != nil
 }
 
 func (fontFace *FontFace) RenderChar(c string) *image.RGBA {
 	defer measure_execution_time("FontFace.RenderChar")()
 	img := image.NewRGBA(image.Rect(0, 0, EditorSingleton.cellWidth, EditorSingleton.cellHeight))
-	fontFace.drawer.Dst = img
-	fontFace.drawer.Dot = fixed.P(0, EditorSingleton.cellHeight-fontFace.descent)
-	// TODO: Check bounds
-	fontFace.drawer.DrawString(c)
+	drawer := font.Drawer{
+		Src:  image.White,
+		Dst:  img,
+		Face: fontFace.handle,
+		Dot:  fixed.P(0, EditorSingleton.cellHeight-fontFace.descent),
+	}
+	drawer.DrawString(c)
 	return img
 }
