75b5832ae18e35fe1fc78d6af7c71ff623ee5653
hismailbulut <bulut-konya@hotmail.com>
2021-07-27 19:35:57 +0300

    Update shader.glsl

--- src/shader.glsl
+++ src/shader.glsl
@@ -70,7 +70,7 @@ void main() {
 		gl_Position    = vec4(pos.xy + (pluspos[i] * pos.zw), 0, 1) * gs_in[0].projection;
 		gs_out.tex1pos = gs_in[0].tex1pos.xy + (pluspos[i] * gs_in[0].tex1pos.zw);
 		gs_out.tex2pos = gs_in[0].tex2pos.xy + (pluspos[i] * gs_in[0].tex2pos.zw);
-		gs_out.ucPos   = gs_in[0].ucPos.xy   + (pluspos[i] * gs_in[0].ucPos.zw);
+		gs_out.ucPos   = gs_in[0].ucPos.xy + (pluspos[i] * gs_in[0].ucPos.zw);
 		gs_out.fgColor = gs_in[0].fgColor;
 		gs_out.bgColor = gs_in[0].bgColor;
 		gs_out.spColor = gs_in[0].spColor;
