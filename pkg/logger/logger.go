f05986014a89b2e8ee40d243db6430fd88ef5927
İsmail Bulut <73897211+hismailbulut@users.noreply.github.com>
2022-08-04 18:36:41 +0300

    Update logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,32 +14,26 @@ import (
 
 var guard sync.Mutex
 var cache struct {
-	name      string
-	version   Version
-	buildtype BuildType
-	file      *os.File
-	color     bool
+	initTime  time.Time // Time when logger was initialized
+	name      string    // Name of the program using this logger
+	version   Version   // Version of the program using this logger
+	buildtype BuildType // Build type of the program using this logger
+	file      *os.File  // File to write logs to
+	color     bool      // Whether to use color in the output
 }
 
 func Init(name string, version Version, buildtype BuildType, color bool) {
 	guard.Lock()
 	defer guard.Unlock()
+	cache.initTime = time.Now()
 	cache.name = name
 	cache.version = version
 	cache.buildtype = buildtype
 	cache.color = color
 }
 
-// Defer this
-func Shutdown() {
-	// If we are panicking print it to logfile.
-	// Also the stack trace will be printed after
-	// fatal error.
-	if pmsg := recover(); pmsg != nil {
-		// Create crash report.
-		Log(FATAL, pmsg)
-	}
-	cleanup()
+func timeString(t time.Time) string {
+	return fmt.Sprintf("%s %d", t.UTC().Format("2006-01-02 15:04:05"), t.UnixMilli())
 }
 
 func InitFile(filename string) {
@@ -52,13 +47,24 @@ func InitFile(filename string) {
 		Log(ERROR, "Failed to get absolute path:", err)
 		return
 	}
-	cache.file, err = os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0666)
+	cache.file, err = os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND|os.O_SYNC, 0666)
 	if err != nil {
 		Log(ERROR, "Failed to create log file:", err)
 		return
 	}
-	// Print informations to log file.
-	cache.file.WriteString(fmt.Sprintln("\n", cache.name, cache.version, cache.buildtype, "LOG", time.Now().UTC()))
+	fmt.Fprintf(cache.file, "%s %s %s LOG %s\n", cache.name, cache.version, cache.buildtype, timeString(time.Now()))
+}
+
+// This function should be deferred after Init because it captures panics
+func Shutdown() {
+	// This will capture the panic and turns it to a fatal
+	if pmsg := recover(); pmsg != nil {
+		Log(FATAL, pmsg)
+	} else {
+		// Log(FATAL) already cleans up, we should only clean up if this is
+		// a regular quit
+		cleanup()
+	}
 }
 
 // This function will always be called.
@@ -67,7 +73,9 @@ func cleanup() {
 	defer guard.Unlock()
 	// If logfile is initialized then close it.
 	if cache.file != nil {
+		fmt.Fprintf(cache.file, "END OF LOG %s\n", timeString(time.Now()))
 		cache.file.Close()
+		cache.file = nil
 	}
 	// Reset terminal color
 	if cache.color {
@@ -75,20 +83,23 @@ func cleanup() {
 	}
 }
 
-func createCrashReport(msg string) {
+func createCrashReport(msg string, isPanic bool) {
 	guard.Lock()
 	defer guard.Unlock()
 	crash_file, err := os.Create("Neoray_crash.log")
 	if err == nil {
 		defer crash_file.Close()
-		crash_file.WriteString(fmt.Sprintln(cache.name, cache.version, cache.buildtype, "Crash Report", time.Now().UTC()))
-		crash_file.WriteString("Please open an issue in github with this file.\n")
-		crash_file.WriteString("The program is crashed because of the following reasons:\n")
-		crash_file.WriteString(msg)
-		crash_file.WriteString("\ngoroutine dump:\n")
-		stackTrace := make([]byte, 1<<15)
+		fmt.Fprintf(crash_file, "%s %s Crash Report\n", cache.name, cache.version)
+		fmt.Fprintf(crash_file, "Build Type: %s\n", cache.buildtype)
+		fmt.Fprintf(crash_file, "Start Time: %s\n", timeString(cache.initTime))
+		fmt.Fprintf(crash_file, "Crash Time: %s\n", timeString(time.Now()))
+		fmt.Fprintf(crash_file, "\n%s %s\n", cache.name, "is crashed because of the following reason:")
+		fmt.Fprintf(crash_file, "%s\n", msg)
+		// Write stack trace
+		fmt.Fprintf(crash_file, "\n%s\n", "Stack trace:")
+		stackTrace := make([]byte, 1<<15) // This is highly enough
 		stackLen := runtime.Stack(stackTrace, true)
-		crash_file.WriteString(string(stackTrace[:stackLen]))
+		crash_file.Write(stackTrace[:stackLen])
 	}
 }
 
@@ -101,26 +112,28 @@ func Log(logLevel LogLevel, message ...any) {
 		return
 	}
 
-	logString := logLevel.String() + " " + fmt.Sprintln(message...)
+	messageStr := strings.TrimRight(fmt.Sprintln(message...), "\n\t ")
+
+	logString := fmt.Sprintf("%s %s", logLevel, messageStr)
 
 	// Print to stdout
 	if cache.color {
-		fmt.Print(string(logLevel.Color()) + logString)
+		fmt.Printf("%s%s\n", string(logLevel.Color()), logString)
 	} else {
-		fmt.Print(logString)
+		fmt.Printf("%s\n", logString)
 	}
 
 	// Print to verbose file if opened
 	guard.Lock()
 	if cache.file != nil {
-		cache.file.WriteString(logString)
+		fmt.Fprintf(cache.file, "%s\n", logString)
 	}
 	guard.Unlock()
 
 	if logLevel == FATAL {
 		// Create crash report file
-		createCrashReport(logString)
-		// Open message box with error
+		createCrashReport(logString, false)
+		// Show error dialog
 		dialog.Message(logString).Error()
 		// Cleanup and shutdown.
 		cleanup()
